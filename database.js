2c16198b0be0588b0e695e762c56ed438669cb34
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-12-09 20:52:06 -0700

    Make mysql2 the default

--- database.js
+++ database.js
@@ -1,37 +1,5 @@
-const mysql = require("mysql");
-const util = require("util");
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  database: process.env.DB,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-});
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      console.error("Database connection was closed.");
-    }
-    if (err.code === "ER_CON_COUNT_ERROR") {
-      console.error("Database has too many connections.");
-    }
-    if (err.code === "ECONNREFUSED") {
-      console.error("Database connection was refused.");
-    }
-  }
-  if (connection) connection.release();
-  return;
-});
-
-pool.query = util.promisify(pool.query);
-
-module.exports = pool;
-
-/*  ON LINUX, REPLACE THE ABOVE WITH THE FOLLOWING:  */
-/*
-
 const mysql = require("mysql2");
+const util = require("util");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -39,5 +7,8 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+});
 
-*/
+pool.query = util.promisify(pool.query);
+
+module.exports = pool;
