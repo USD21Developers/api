d8b2595ee5d5d10fa30fe4570613f9f4a1c0b845
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2021-01-01 12:05:44 -0700

    Add route for beginning a Paypal subscription

--- routes/controllers_fp/begin-subscription.js
+++ routes/controllers_fp/begin-subscription.js
@@ -1,4 +1,4 @@
-const moment = require("moment-timezone");
+const paypal = require("paypal-rest-sdk");
 
 exports.POST = (req, res) => {
   // Enforce authorization
@@ -11,15 +11,93 @@ exports.POST = (req, res) => {
       msgType: "error",
     });
   }
+
+  // Price
+  const price = "9.99";
+
+  // Request parameters
+  const productName = req.body.productName || "First Principles App";
+  const productSku = req.body.productSku || "fp-app-subscription";
+  const productDescription =
+    req.body.productDescription || "Annual Subscription";
+  const lang = req.body.lang || "en";
+
+  // Determine whether on local or staging
+  const isLocal = req.headers.referer.indexOf("localhost") >= 0 ? true : false;
   const isStaging = req.headers.referer.indexOf("staging") >= 0 ? true : false;
+
+  // Set database
   const db = isStaging
     ? require("../../database-test")
     : require("../../database");
 
-  const timeZone = req.body.timeZone;
-  const timeZoneOffset = moment.tz(timeZone).format("Z:00").slice(0, -3);
+  // Configure Paypal
 
-  return res
-    .status(200)
-    .send({ msg: "Beginning subscription...", msgType: "success" });
+  if (process.env.ENV === "development" || isStaging) {
+    paypal.configure({
+      mode: "sandbox",
+      client_id: process.env.FP_PAYPAL_SANDBOX_CLIENT_ID,
+      client_secret: process.env.FP_PAYPAL_SANDBOX_SECRET,
+    });
+  } else {
+    paypal.configure({
+      mode: "live",
+      client_id: process.env.FP_PAYPAL_LIVE_CLIENT_ID,
+      client_secret: process.env.FP_PAYPAL_LIVE_SECRET,
+    });
+  }
+
+  let host = `https://firstprinciples.mobi`;
+  if (isLocal) host = "http://localhost:5000";
+  if (isStaging) host = "https://staging.firstprinciples.mobi";
+
+  let returnUrl = `${host}/lang/${lang}/account/subscribe/success/`;
+  let cancelUrl = `${host}/lang/${lang}/account/subscribe/cancel/`;
+
+  const create_payment_json = {
+    intent: "sale",
+    payer: {
+      payment_method: "paypal",
+    },
+    redirect_urls: {
+      return_url: returnUrl,
+      cancel_url: cancelUrl,
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: productName,
+              sku: productSku,
+              price: price,
+              currency: "USD",
+              quantity: 1,
+            },
+          ],
+        },
+        amount: {
+          currency: "USD",
+          total: price,
+        },
+        description: productDescription,
+      },
+    ],
+  };
+
+  paypal.payment.create(create_payment_json, (error, payment) => {
+    if (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .send({ msg: "unable to create paypal payment", msgType: "error" });
+    } else {
+      console.log(require("util").inspect(payment, true, 7, true));
+      res.status(200).send({
+        msg: "paypal payment created",
+        msgType: "success",
+        payment: payment,
+      });
+    }
+  });
 };
