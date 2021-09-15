5fe85427b59fa4547ea8f2c2fe8184d6edd01919
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2021-09-15 12:10:29 -0700

    Refactor shape of data

--- routes/controllers_services/churches.js
+++ routes/controllers_services/churches.js
@@ -19,21 +19,91 @@ exports.GET = (req, res) => {
 
   db.query(sql, [], (err, result) => {
     if (err) {
-      return res.status(500).send({ msg: "unable to query service for churches", msgType: "error" });
+      return res.status(500).send({
+        msg: "unable to query service for churches",
+        msgType: "error",
+      });
     }
 
-    const data = result.map(item => {
-      const { church_name, church_URL, mailing_city, mailing_state, mailing_country, country_iso } = item;
-      return {
-        name: church_name,
-        url: church_URL,
-        city: mailing_city,
-        state: mailing_state,
-        country: mailing_country,
-        country_iso: country_iso
+    const churches = [];
+    result.map((item) => {
+      const {
+        church_name,
+        church_URL,
+        mailing_city,
+        mailing_state,
+        mailing_country,
+        country_iso,
+      } = item;
+      let place = mailing_city.trim();
+      if (
+        mailing_state.trim() !== "" &&
+        mailing_state.trim() !== mailing_city
+      ) {
+        place = `${mailing_city}, ${mailing_state}`.trim();
+      }
+      if (!churches.length) {
+        churches.push({
+          country: {
+            iso: country_iso,
+            name: mailing_country,
+            churches: [
+              {
+                name: church_name,
+                place: place,
+                url: church_URL,
+              },
+            ],
+          },
+        });
+        return;
+      }
+      const churchesLen = churches.length;
+      let hasCountry = false;
+      for (let i = 0; i < churchesLen; i++) {
+        const church = churches[i];
+        if (church.country.iso === country_iso) {
+          hasCountry = true;
+          break;
+        }
+      }
+      if (!hasCountry) {
+        churches.push({
+          country: {
+            iso: country_iso,
+            name: mailing_country,
+            churches: [
+              {
+                name: church_name,
+                place: place,
+                url: church_URL,
+              },
+            ],
+          },
+        });
+      } else {
+        for (let i = 0; i < churchesLen; i++) {
+          if (churches[i].country.iso === country_iso) {
+            const churchesInCountryLen = churches[i].country.churches.length;
+            let containsChurch = false;
+            for (let j = 0; j < churchesInCountryLen; j++) {
+              if (!churches[i].country.churches[j].name === church_name) {
+                containsChurch = true;
+              }
+            }
+            if (!containsChurch) {
+              churches[i].country.churches.push({
+                name: church_name,
+                place: place,
+                url: church_URL,
+              });
+              continue;
+            }
+          }
+        }
       }
     });
 
-    return res.status(200).send(data);
+    return res.status(200).send(churches);
   });
-}
+};
