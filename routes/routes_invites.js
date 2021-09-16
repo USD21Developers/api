ef36fbca76459994d5f134cfcddacf112abc267b
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2021-09-16 11:46:03 -0700

    Enable forgot password route

--- routes/routes_invites.js
+++ routes/routes_invites.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const utils = require("./controllers_invites/utils");
 const authenticateToken = utils.authenticateToken;
 
-
 // REGISTRATION
 
 const register = require("./controllers_invites/register");
@@ -11,7 +10,6 @@ router.post("/register", register.POST);
 
 const registerConfirm = require("./controllers_invites/register-confirm");
 router.post("/register-confirm", registerConfirm.POST);
-
 
 // SECURITY
 
@@ -21,10 +19,10 @@ router.post("/refresh-token", refreshToken.POST);
 const login = require("./controllers_invites/login");
 router.post("/login", login.POST);
 
-/* const forgotPassword = require("./controllers_invites/forgot-password");
+const forgotPassword = require("./controllers_invites/forgot-password");
 router.post("/forgot-password", forgotPassword.POST);
 
-const resetPassword = require("./controllers_invites/reset-password");
+/* const resetPassword = require("./controllers_invites/reset-password");
 router.post("/reset-password", resetPassword.POST);
 
 const passwordMustChange = require("./controllers_invites/password-must-change");
@@ -33,7 +31,6 @@ router.post(
   authenticateToken,
   passwordMustChange.POST
 ); */
-
 
 // AUTHORIZATION
 
