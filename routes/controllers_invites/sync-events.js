31c872441350d156c31a516539c76cc8d66615fe
Jason McNeill <vrtjason@gmail.com>
2022-06-28 12:41:24 -0700

    Add controller to sync events

---
+++ routes/controllers_invites/sync-events.js
@@ -0,0 +1,30 @@
+const moment = require("moment");
+const momentTimeZone = require("moment-timezone");
+
+exports.GET = (req, res) => {
+  // Enforce authorization
+  const usertype = req.user.usertype;
+  const allowedUsertypes = ["sysadmin", "user"];
+  let isAuthorized = false;
+  if (allowedUsertypes.includes(usertype)) isAuthorized = true;
+  if (req.user.may_create_coupons) isAuthorized = true;
+  if (!isAuthorized) {
+    console.log(`User (userid ${req.user.userid}) is not authorized.`);
+    return res.status(401).send({
+      msg: "user is not authorized for this action",
+      msgType: "error",
+    });
+  }
+
+  // Set database
+  const isStaging = req.headers.referer.indexOf("staging") >= 0 ? true : false;
+  const db = isStaging
+    ? require("../../database-invites-test")
+    : require("../../database-invites");
+
+  // Test response
+  const eventsTest = [{ eventid: 1 }, { eventid: 2 }, { eventid: 3 }];
+  return res.status(200).send({
+    msg: "events synced", msgType: "success", events: eventsTest
+  });
+};
