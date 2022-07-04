7c3d914fee8758f18f8186b7b62dc981862c8d50
Jason McNeill <vrtjason@gmail.com>
2022-07-04 11:17:53 -0700

    Debug

--- routes/controllers_invites/test-dates.js
+++ routes/controllers_invites/test-dates.js
@@ -2,8 +2,8 @@ const moment = require("moment-timezone");
 
 exports.GET = (req, res) => {
   const dates = {};
-  dates.church = moment.tz("2022-07-10 10:00 AM", "America/Phoenix").format();
-  dates.churchUtc = moment.utc(dates.church).format("YYYY-MM-D H:mm A");
+  dates.local = moment.tz(moment("2022-07-10 10:00 AM").format(), "America/Phoenix").format();
+  dates.utc = moment.utc(dates.church).format("YYYY-MM-D H:mm A");
 
   return res.status(200).send(dates);
 };
