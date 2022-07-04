02544c33c6329ea79da24c4f804b82be897d4dfe
Jason McNeill <vrtjason@gmail.com>
2022-07-04 11:52:14 -0700

    Debug

--- routes/controllers_invites/test-dates.js
+++ routes/controllers_invites/test-dates.js
@@ -5,5 +5,5 @@ exports.GET = (req, res) => {
   const localDateTime = moment.tz("2022-07-10 10:00 AM", timezone);
   const utcDateTime = moment.tz(localDateTime, "utc");
 
-  return res.status(200).send([localDateTime.format(), utcDateTime.format()]);
+  return res.status(200).send([localDateTime.format("YYYY-MM-DD HH:mm"), utcDateTime.format("YYYY-MM-DD HH:mm")]);
 };
