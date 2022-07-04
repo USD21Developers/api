edad1eb3cd7cc2ce48f86a41199e6ac48d83e48a
Jason McNeill <vrtjason@gmail.com>
2022-07-04 11:40:10 -0700

    Debug

--- routes/controllers_invites/test-dates.js
+++ routes/controllers_invites/test-dates.js
@@ -3,7 +3,7 @@ const moment = require("moment-timezone");
 exports.GET = (req, res) => {
   const timezone = "America/Phoenix";
   const localDateTime = moment.tz("2022-07-10 10:00 AM", timezone);
-  const utcDateTime = moment.tz(localDateTime, "utc").format("h:mm A");
+  const utcDateTime = moment.tz(localDateTime, "utc");
 
-  return res.status(200).send(["10:00 AM", utcDateTime]);
+  return res.status(200).send([localDateTime.format(), utcDateTime.format()]);
 };
