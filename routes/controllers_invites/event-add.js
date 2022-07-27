e6b2f145e463394b09a252c3122cbcdf218e1fe1
Jason McNeill <vrtjason@gmail.com>
2022-07-26 21:14:51 -0700

    Fix bugs

--- routes/controllers_invites/event-add.js
+++ routes/controllers_invites/event-add.js
@@ -473,7 +473,7 @@ exports.POST = (req, res) => {
       line1: addressLine1.trim().length ? addressLine1.trim() : null,
       line2: addressLine2.trim().length ? addressLine2.trim() : null,
       line3: addressLine3.trim().length ? addressLine3.trim() : null,
-      coordinates: (latitude.trim().length && longitude.trim().length) ? `ST_GeomFromText( 'POINT(${latitude.trim()},${longitude.trim()})' )` : null
+      coordinates: (latitude.trim().length && longitude.trim().length) ? `ST_GeomFromText( POINT(${latitude.trim()},${longitude.trim()}) )` : null
     };
 
     const sqlDuration = duration.trim().length ? duration.trim() : null;
