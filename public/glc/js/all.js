97d99b1e7c75ea6e877402cbbb56aff4fe10af89
Jason McNeill <vrtjason@gmail.com>
2022-07-30 09:33:26 -0700

    Add next session

--- public/glc/js/all.js
+++ public/glc/js/all.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+function nextSession() {
+  const el = document.querySelector("#nextsession");
+  if (!el) return;
+
+  const upcomingEventsHTML = `&lt; There are no upcoming events. &gt;`;
+
+  el.innerHTML = upcomingEventsHTML;
+}
+
+function init() {
+  nextSession();
+}
+
+init();
