98bd293f80049298b53b72e6e8f657590561af4a
Jason McNeill <vrtjason@gmail.com>
2022-08-08 20:48:45 -0700

    Add global front-end script

---
+++ public/glc/admin/all.js
@@ -0,0 +1,62 @@
+function getAccessToken() {
+  let needToRefresh = false;
+  const accessToken = sessionStorage.getItem("accessToken") || "";
+  const now = Date.now().valueOf() / 1000;
+  let expiry = now;
+  try {
+    expiry = JSON.parse(atob(accessToken.split(".")[1])).exp;
+    if (expiry < now) needToRefresh = true;
+  } catch (err) {
+    needToRefresh = true;
+  }
+  return new Promise((resolve, reject) => {
+    if (!needToRefresh) return resolve(accessToken);
+    const refreshToken = localStorage.getItem("refreshToken") || "";
+    if (!refreshToken.length) return reject("refresh token missing");
+
+    const endpoint = `${getApiHost()}/refresh-token`;
+
+    fetch(endpoint, {
+      mode: "cors",
+      method: "POST",
+      body: JSON.stringify({
+        refreshToken: refreshToken,
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        switch (data.msg) {
+          case "tokens renewed":
+            const { accessToken, refreshToken } = data;
+            localStorage.setItem("refreshToken", refreshToken);
+            sessionStorage.setItem("accessToken", accessToken);
+            resolve(accessToken);
+            break;
+          default:
+            resolve("could not get access token");
+            break;
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+}
+
+function getApiHost() {
+  let host;
+
+  switch (window.location.hostname) {
+    case "api.usd21.org":
+      host = "https://api.usd21.org/glc";
+      break;
+    default:
+      host = `${window.location.protocol}//${window.location.host}/glc`;
+      break;
+  }
+
+  return host;
+}
