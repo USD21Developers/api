0e2375946e3b74ec1186ce0b8c7e57651948e0ff
Jason McNeill <vrtjason@gmail.com>
2022-08-10 13:12:43 -0700

    Add data

--- public/glc/js/all.js
+++ public/glc/js/all.js
@@ -1,3 +1,251 @@
+const program = [
+  {
+    title: "The ICCM Chancellor’s Gala",
+    location: "Anaheim Convention Center  Level 201A",
+    date: "Wednesday - August 10th",
+    time: "6:30 PM",
+    datetime: new Date("2022-08-10T18:30:00")
+  },
+  {
+    title: "Church Builders Workshop",
+    location: "Anaheim Hilton - Pacific Ballroom C & D ",
+    date: "Thursday - August 11th",
+    time: "9:00 AM",
+    datetime: new Date("2022-08-11T09:00:00")
+  },
+  {
+    title: "MERCYWORLDWIDE Summit",
+    location: "Anaheim Convention Center - Level 2 - Room 204B",
+    date: "Thursday - August 11th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-11T14:00:00")
+  },
+  {
+    title: "CyberMinistry Seminar",
+    location: "Anaheim Convention Center - Level 2 - Room 207AB",
+    date: "Thursday - August 11th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-11T14:00:00")
+  },
+  {
+    title: "Administration And Law Congress",
+    location: "Anaheim Convention Center - Level 2 - Room 204 C",
+    date: "Thursday - August 11th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-11T14:00:00")
+  },
+  {
+    title: "Great Physician Ministry (Formerly Refined By Fire Ministry)",
+    location: "Anaheim Convention Center - Level 2 - Room 207 CD",
+    date: "Thursday - August 11th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-11T14:00:00")
+  },
+  {
+    title: "GLC First General Session",
+    location: "Anaheim Convention Center - North 100 Level - Exhibition Hall",
+    date: "Thursday - August 11th",
+    time: "7:30 PM",
+    datetime: new Date("2022-08-11T19:30:00")
+  },
+  {
+    title: "ICCM Commencement Rehearsal And Pictures",
+    location: "Anaheim Convention Center - North 100 Level - Exhibition Hall",
+    date: "Friday - August 12th",
+    time: "6:30 AM",
+    datetime: new Date("2022-08-12T06:30:00")
+  },
+  {
+    title: "Africanus World Sector",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 259 AB",
+    date: "Friday - August 12th",
+    time: "9:30 AM",
+    datetime: new Date("2022-08-12T09:30:00")
+  },
+  {
+    title: "Austral-China World Sector",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 262 ABC",
+    date: "Friday - August 12th",
+    time: "9:30 AM",
+    datetime: new Date("2022-08-12T09:30:00")
+  },
+  {
+    title: "European World Sector",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 253 ABC",
+    date: "Friday - August 12th",
+    time: "9:30 AM",
+    datetime: new Date("2022-08-12T09:30:00")
+  },
+  {
+    title: "Latin America World Sector",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 261 ABC",
+    date: "Friday - August 12th",
+    time: "9:30 AM",
+    datetime: new Date("2022-08-12T09:30:00")
+  },
+  {
+    title: "Middle East World Sector",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 264 ABC",
+    date: "Friday - August 12th",
+    time: "9:30 AM",
+    datetime: new Date("2022-08-12T09:30:00")
+  },
+  {
+    title: "PACK And Eurasian World Sectors",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 255, 257 ABC",
+    date: "Friday - August 12th",
+    time: "9:30 AM",
+    datetime: new Date("2022-08-12T09:30:00")
+  },
+  {
+    title: "SAGES World Sector",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 251 ABC",
+    date: "Friday - August 12th",
+    time: "9:30 AM",
+    datetime: new Date("2022-08-12T09:30:00")
+  },
+  {
+    title: "Tribe Pacific Rim World Sector",
+    location: "Anaheim Convention Center - North 100 Level - Exhibition Hall",
+    date: "Friday - August 12th",
+    time: "9:30 AM",
+    datetime: new Date("2022-08-12T09:30:00")
+  },
+  {
+    title: "AMS Session",
+    location: "Anaheim Convention Center - North 100 Level - Exhibition Hall",
+    date: "Friday - August 12th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-12T14:00:00")
+  },
+  {
+    title: "Song Leaders Session",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 264 ABC",
+    date: "Friday - August 12th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-12T14:00:00")
+  },
+  {
+    title: "Chemical Recovery Session",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 262 ABC",
+    date: "Friday - August 12th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-12T14:00:00")
+  },
+  {
+    title: "Emotional Recovery Session",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 253 ABC",
+    date: "Friday - August 12th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-12T14:00:00")
+  },
+  {
+    title: "Life Skills Session",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 259 AB",
+    date: "Friday - August 12th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-12T14:00:00")
+  },
+  {
+    title: "Single Parents Session",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 251 ABC",
+    date: "Friday - August 12th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-12T14:00:00")
+  },
+  {
+    title: "Kingdom Kids Session",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 263 B",
+    date: "Friday - August 12th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-12T14:00:00")
+  },
+  {
+    title: "GLC MERCY Night - Second General Session",
+    location: "Friday - August 12th",
+    date: "Anaheim Convention Center - North 100 Level - Exhibition Hall",
+    time: "7:30 PM",
+    datetime: new Date("2022-08-12T19:30:00")
+  },
+  {
+    title: "Teen Ministry",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 262 ABC",
+    date: "Saturday - August 13th",
+    time: "10:00 AM",
+    datetime: new Date("2022-08-13T10:00:00")
+  },
+  {
+    title: "Campus Ministry",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 255 ABC / 257",
+    date: "Saturday - August 13th",
+    time: "10:00 AM",
+    datetime: new Date("2022-08-13T10:00:00")
+  },
+  {
+    title: "Singles Ministry",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 251 ABC / 252 ABC",
+    date: "Saturday - August 13th",
+    time: "10:00 AM",
+    datetime: new Date("2022-08-13T10:00:00")
+  },
+  {
+    title: "Marrieds Ministry",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 261 AB / 258 AB / 256 AB",
+    date: "Saturday - August 13th",
+    time: "10:00 AM",
+    datetime: new Date("2022-08-13T10:00:00")
+  },
+  {
+    title: "Spanish-Speaking Disciples Ministry",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 264 ABC",
+    date: "Saturday - August 13th",
+    time: "10:00 AM",
+    datetime: new Date("2022-08-13T10:00:00")
+  },
+  {
+    title: "Disciples Of Wisdom Ministry",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 263B",
+    date: "Saturday - August 13th",
+    time: "10:00 AM",
+    datetime: new Date("2022-08-13T10:00:00")
+  },
+  {
+    title: "Shepherding Conclave",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 253 ABC",
+    date: "Saturday - August 13th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-13T14:00:00")
+  },
+  {
+    title: "Coming Home Ministry (Versus 'Coming Out')",
+    location: "Anaheim Convention Center - North 200 Level - Rooms 259 AB",
+    date: "Saturday - August 13th",
+    time: "2:00 PM",
+    datetime: new Date("2022-08-13T14:00:00")
+  },
+  {
+    title: "Kingdom Banquet",
+    location: "Anaheim Convention Center - North 100 Level - Exhibition Hall",
+    date: "Saturday - August 13th",
+    time: "6:00 PM",
+    datetime: new Date("2022-08-13T18:00:00")
+  },
+  {
+    title: "Dancing With The Stars - Kingdom Pure Dance",
+    location: "Anaheim Convention Center - North 100 Level - Exhibition Hall",
+    date: "Saturday - August 13th",
+    time: "9:00 PM",
+    datetime: new Date("2022-08-13T21:00:00")
+  },
+  {
+    title: "Congregational Sunday Worship Service",
+    location: "Anaheim Convention Center - North 100 Level - Exhibition Hall",
+    date: "Sunday - August 14th",
+    time: "10:00 AM",
+    datetime: new Date("2022-08-14T10:00:00")
+  }
+]
+
 function nextSession() {
   const el = document.querySelector(".nextsession");
   if (!el) return;
@@ -7,6 +255,14 @@ function nextSession() {
   linkTag.setAttribute("rel", "stylesheet");
   linkTag.setAttribute("href", "https://api.usd21.org/glc/css/nextsession.css");
   head.append(linkTag);
+
+  const momentJS = document.querySelector("script");
+  momentJS.src = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js";
+  linkTag.append(momentJS);
+
+  momentTimezone = document.querySelector("script");
+  momentTimezone.src = "https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.34/moment-timezone.min.js";
+  linkTag.append(momentTimezone);
 
   const contentEl = document.createElement("div");
   contentEl.setAttribute("id", "nextSessionContent");
