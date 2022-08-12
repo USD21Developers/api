2f65a1f3d7396ddcc525c74877220362f9d2f1ea
Jason McNeill <vrtjason@gmail.com>
2022-08-12 00:48:39 -0700

    Troubleshoot

--- public/glc/js/all.js
+++ public/glc/js/all.js
@@ -286,11 +286,11 @@ function nextSession() {
   const el = document.querySelector(".nextsession");
   if (!el) return;
 
-  const head = document.querySelector("head");
+  /* const head = document.querySelector("head");
   const linkTag = document.createElement("link");
   linkTag.setAttribute("rel", "stylesheet");
   linkTag.setAttribute("href", "https://api.usd21.org/glc/css/nextsession.css");
-  head.append(linkTag);
+  head.append(linkTag); */
 
   const contentEl = document.createElement("div");
   contentEl.setAttribute("id", "nextSessionContent");
