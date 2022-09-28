4b0855b0764d184b94d2a519be887a9149c68bd6
Jason McNeill <vrtjason@gmail.com>
2022-09-28 12:33:33 -0700

    Increase size of uploads

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set("view engine", "ejs");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
