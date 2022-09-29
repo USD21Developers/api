63809a5c236e343edcff5923a4e0909338906ac7
Jason McNeill <vrtjason@gmail.com>
2022-09-29 13:17:16 -0700

    Add function to store profile photo

--- routes/controllers_invites/utils.js
+++ routes/controllers_invites/utils.js
@@ -384,3 +384,26 @@ exports.getDistance = (db, originObj, destinationObj) => {
       })
   });
 }
+
+exports.storeProfileImage = (userid, base64Image) => {
+  const AWS = require("aws-sdk");
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.INVITES_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.INVITES_AWS_SECRET_ACCESS_KEY
+  });
+  const filename = `profile_400_${userid}.jpg`;
+  const fileContent = new Buffer.from(base64Image.replace(/^data:image\/\w+;base64,/, ""), "base64");
+  const params = {
+    Bucket: process.env.INVITES_AWS_BUCKET_NAME,
+    Key: filename,
+    Body: fileContent
+  };
+
+  s3.upload(params, (err, data) => {
+    if (err) {
+      reject(err);
+    }
+
+    resolve(data.Location);
+  });
+}
