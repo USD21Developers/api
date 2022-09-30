688957a4146d65fa7240d6ab78bd74c25bdb38ba
Jason McNeill <vrtjason@gmail.com>
2022-09-30 14:39:16 -0700

    Modify profile filename

--- routes/controllers_invites/utils.js
+++ routes/controllers_invites/utils.js
@@ -392,7 +392,7 @@ exports.storeProfileImage = (userid, base64Image) => {
       accessKeyId: process.env.INVITES_AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.INVITES_AWS_SECRET_ACCESS_KEY
     });
-    const filename = `profile_400_${userid}.jpg`;
+    const filename = `${userid}_400.jpg`;
     const fileContent = new Buffer.from(base64Image.replace(/^data:image\/\w+;base64,/, ""), "base64");
     const params = {
       Bucket: process.env.INVITES_AWS_BUCKET_NAME,
