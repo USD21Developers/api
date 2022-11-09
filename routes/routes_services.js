fff88bde75e44291807f765b1c2c4e46b3a0993d
Jason McNeill <vrtjason@gmail.com>
2022-11-08 20:45:16 -0700

    Fix bug

--- routes/routes_services.js
+++ routes/routes_services.js
@@ -11,6 +11,6 @@ const churchDirectory = require("./controllers_services/church-directory");
 router.get("/church-directory", churchDirectory.GET);
 
 const countryNames = require("./controllers_services/country-names");
-router.get("/country-names", countryNames.POST);
+router.post("/country-names", countryNames.POST);
 
 module.exports = router;
