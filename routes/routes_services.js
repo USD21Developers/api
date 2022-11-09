7cfbd98f5c00d46c4b8550ee9a02100f651c0b84
Jason McNeill <vrtjason@gmail.com>
2022-11-09 16:21:00 -0700

    Reorganize routes

--- routes/routes_services.js
+++ routes/routes_services.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const churches = require("./controllers_services/churches");
-router.get("/churches", churches.GET);
-
 const geotagip = require("./controllers_services/geotagip");
 router.get("/geotagip", geotagip.GET);
+
+const churches = require("./controllers_services/churches");
+router.get("/churches", churches.GET);
 
 const churchDirectory = require("./controllers_services/church-directory");
 router.get("/church-directory", churchDirectory.GET);
