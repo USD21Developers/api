bd0cfc4a08a3b74e4629edc77f4da86bb59b32d7
Jason McNeill <vrtjason@gmail.com>
2022-11-14 13:58:48 -0700

    Build out church directory

--- routes/controllers_services/church-directory.js
+++ routes/controllers_services/church-directory.js
@@ -1,25 +1,29 @@
 exports.GET = (req, res) => {
   const db = require("../../database-services");
 
-  const sqlChurchesByCountry = `
+  const sql = `
     SELECT 
       churchID,
       church_name,
       church_URL,
-      identifying_place,
+      contact_name,
+      contact_number,
+      contact_image,
       mailing_city,
       mailing_state,
       mailing_country,
+      identifying_place,
       country_iso
     FROM 
       churches
     ORDER BY
-      mailing_country,
-      identifying_place
+      country_iso,
+      identifying_place,
+      church_name
     ;
   `;
 
-  db.query(sqlChurchesByCountry, [], (err, result) => {
+  db.query(sql, [], (err, result) => {
     if (err) {
       console.log(err);
       return res.status(500).send({
@@ -28,121 +32,21 @@ exports.GET = (req, res) => {
       });
     }
 
-    const churches = [];
-    result.map((item) => {
-      const {
-        churchID,
-        church_name,
-        church_URL,
-        identifying_place,
-        mailing_city,
-        mailing_country,
-        country_iso,
-      } = item;
-      let countryName = mailing_country;
-      if (country_iso === "us") countryName = "United States";
-      let place = identifying_place;
-      if (typeof place === "string" && !place.length) place = mailing_city;
-      if (!churches.length) {
-        churches.push({
-          country: {
-            iso: country_iso,
-            name: countryName,
-            churches: [
-              {
-                id: churchID,
-                name: church_name,
-                place: place,
-                url: church_URL,
-              },
-            ],
-          },
-        });
-        return;
+    const data = result.map((item) => {
+      let imageURL = "";
+      if (typeof item.contact_image === "object") {
+        imageURL = `https://www.upsidedown21.org/1.1/images/church_leaders/${item.churchID}.jpg?ver=1.6.2`;
       }
-      const churchesLen = churches.length;
-      let hasCountry = false;
-      for (let i = 0; i < churchesLen; i++) {
-        const church = churches[i];
-        if (church.country.iso === country_iso) {
-          hasCountry = true;
-          break;
-        }
-      }
-      if (!hasCountry) {
-        churches.push({
-          country: {
-            iso: country_iso,
-            name: countryName,
-            churches: [
-              {
-                id: churchID,
-                name: church_name,
-                place: place,
-                url: church_URL,
-              },
-            ],
-          },
-        });
-      } else {
-        for (let i = 0; i < churchesLen; i++) {
-          if (churches[i].country.iso === country_iso) {
-            const churchesInCountryLen = churches[i].country.churches.length;
-            let containsChurch = false;
-            for (let j = 0; j < churchesInCountryLen; j++) {
-              if (!churches[i].country.churches[j].name === church_name) {
-                containsChurch = true;
-              }
-            }
-            if (!containsChurch) {
-              churches[i].country.churches.push({
-                id: churchID,
-                name: church_name,
-                place: place,
-                url: church_URL,
-              });
-              continue;
-            }
-          }
-        }
-      }
+
+      item.contact_image = imageURL;
+
+      return item;
     });
 
-    return res.status(200).send(churches);
-  });
-};
-
-exports.FETCH = async () => {
-  const db = require("../../database-services");
-
-  const sqlChurches = `
-    SELECT
-      churchID,
-      church_name,
-      mailing_city,
-      mailing_state,
-      mailing_country,
-      country_iso
-    FROM
-      churches
-    ORDER BY
-      country_iso,
-      mailing_country,
-      mailing_state,
-      mailing_city,
-      church_name,
-      churchID
-    ;
-  `;
-
-  return new Promise((resolve, reject) => {
-    db.query(sqlChurches, [], (err, result) => {
-      if (err) {
-        console.log(err);
-        return reject(new Error("unable to query service for churches"));
-      }
-
-      resolve(result);
+    return res.status(200).send({
+      msg: "churches retrieved",
+      msgType: "success",
+      churches: data,
     });
   });
 };
