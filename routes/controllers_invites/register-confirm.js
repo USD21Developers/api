204cd90eb7140fc3386fdab871b416bbb67b656a
Jason McNeill <vrtjason@gmail.com>
2023-03-24 17:42:10 -0700

    Require login after registration confirmation

--- routes/controllers_invites/register-confirm.js
+++ routes/controllers_invites/register-confirm.js
@@ -70,7 +70,6 @@ exports.POST = (req, res) => {
 
     // Token valid; update database
 
-    const userid = result[0].userid || 0;
     const sql = `
       UPDATE
         tokens t
@@ -91,57 +90,10 @@ exports.POST = (req, res) => {
           .send({ msg: "unable to update token record", msgType: "error" });
       }
 
-      const sql = `SELECT firstname, lastname, usertype, passwordmustchange FROM users WHERE userid = ? LIMIT 1;`;
-      db.query(sql, [userid], (err, result) => {
-        if (err) {
-          console.log(err);
-          return res
-            .status(500)
-            .send({ msg: "unable to query for user", msgType: "error" });
-        }
-
-        if (!result.length) {
-          return res
-            .status(404)
-            .send({ msg: "user not found", msgType: "error" });
-        }
-
-        // Registration confirmed; send JWT
-        const jsonwebtoken = require("jsonwebtoken");
-        const firstname = result[0].firstname || "";
-        const lastname = result[0].lastname || "";
-        const usertype = result[0].usertype || "user";
-        const passwordmustchange = result[0].passwordmustchange || 0;
-        const refreshToken = jsonwebtoken.sign(
-          {
-            firstname: firstname,
-            lastname: lastname,
-            userid: userid,
-            usertype: usertype,
-            passwordmustchange: passwordmustchange == 1 ? true : 0,
-          },
-          process.env.REFRESH_TOKEN_SECRET,
-          { expiresIn: "90d" }
-        );
-
-        const accessToken = jsonwebtoken.sign(
-          {
-            firstname: firstname,
-            lastname: lastname,
-            userid: userid,
-            usertype: usertype,
-            passwordmustchange: passwordmustchange == 1 ? true : 0,
-          },
-          process.env.ACCESS_TOKEN_SECRET,
-          { expiresIn: "1d" }
-        );
-
-        return res.status(200).send({
-          msg: "registration confirmed",
-          msgType: "success",
-          refreshToken: refreshToken,
-          accessToken: accessToken,
-        });
+      // Registration confirmed
+      return res.status(200).send({
+        msg: "registration confirmed",
+        msgType: "success",
       });
     });
   });
