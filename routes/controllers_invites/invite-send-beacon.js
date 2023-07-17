b46310e7e360e677517a646fa7f1224494b8927c
Jason McNeill <vrtjason@gmail.com>
2023-07-17 11:39:24 -0700

    FIx bug

--- routes/controllers_invites/invite-send-beacon.js
+++ routes/controllers_invites/invite-send-beacon.js
@@ -66,7 +66,7 @@ exports.POST = (req, res) => {
       let point = null;
       if (coords !== null) {
         const { lat, long } = coords;
-        point = `ST_GeomFromText(POINT(${lat} ${long}))`;
+        point = ST_GeomFromText(`POINT(${lat} ${long})`);
       }
 
       const encryptedSms = recipientsms ? JSON.stringify(recipientsms) : null;
