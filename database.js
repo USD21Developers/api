29ee0a352d1a31f345b7511c14ee0ab0fe635301
Jason McNeill <vrtjason@gmail.com>
2023-09-18 16:10:13 -0700

    Enable storing emojis in DB

--- database.js
+++ database.js
@@ -6,6 +6,8 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  charset: "utf8mb4",
+  collation: "utf8mb4_unicode_ci",
 });
 
 pool.getConnection((err, connection) => {
