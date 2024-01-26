a9550ddd9d45b5e521f48ba0d6c342f436b2648c
Jason McNeill <vrtjason@gmail.com>
2024-01-26 15:39:18 -0700

    Enable unsubscribe for both invite and app

--- routes/controllers_invites/unsubscribe.js
+++ routes/controllers_invites/unsubscribe.js
@@ -1,5 +1,98 @@
 const jsonwebtoken = require("jsonwebtoken");
 
+function unsubscribeFromInvite(db, invitationid, userid) {
+  return new Promise((resolve, reject) => {
+    const sql = `
+      UPDATE
+        invitations
+      SET
+        unsubscribedFromEmail = 1,
+        unsubscribedFromEmailAt = UTC_TIMESTAMP(),
+        unsubscribedFromPush = 1,
+        unsubscribedFromPushAt = UTC_TIMESTAMP()
+      WHERE
+        userid = ?
+      AND
+        invitationid = ?
+      ;
+    `;
+
+    db.query(sql, [userid, invitationid], (error, result) => {
+      if (error) {
+        console.log(error);
+        return reject(error);
+      }
+
+      const resultObj = {
+        affectedRows: result.affectedRows,
+        changedRows: result.changedRows,
+      };
+
+      return resolve(resultObj);
+    });
+  });
+}
+
+function unsubscribeFromRecipient(db, invitationids, invitationid, userid) {
+  return new Promise((resolve, reject) => {
+    if (!invitationids.includes(invitationid)) {
+      const errMsg = "invitationid must match the invitationid in the JWT";
+      console.log(errMsg);
+      return reject(new Error(errMsg));
+    }
+
+    const sql = `
+      UPDATE
+        invitations
+      SET
+        unsubscribedFromEmail = 1,
+        unsubscribedFromEmailAt = UTC_TIMESTAMP(),
+        unsubscribedFromPush = 1,
+        unsubscribedFromPushAt = UTC_TIMESTAMP()
+      WHERE
+        userid = ?
+      AND
+        invitationid IN ?
+    `;
+
+    db.query(sql, [userid, [invitationids]], (error, result) => {
+      if (error) {
+        return reject(error);
+      }
+
+      const resultObj = {
+        affectedRows: result.affectedRows,
+        changedRows: result.changedRows,
+      };
+
+      return resolve(resultObj);
+    });
+  });
+}
+
+function unsubscribeFromApp(db, userid) {
+  return new Promise((resolve, reject) => {
+    const sql = `
+      UPDATE users
+      SET settings = JSON_SET(settings, '$.enablePushNotifications', false, '$.enableEmailNotifications', false)
+      WHERE userid = ?;    
+    `;
+
+    db.query(sql, [userid], (error, result) => {
+      if (error) {
+        return reject(error);
+      }
+
+      const resultObj = {
+        affectedRows: result.affectedRows,
+        changedRows: result.changedRows,
+      };
+
+      return resolve(resultObj);
+    });
+  });
+}
+
 exports.POST = (req, res) => {
   // Set database
   const isStaging =
@@ -10,16 +103,6 @@ exports.POST = (req, res) => {
 
   // Parameters
   const { invitationids, jwt, unsubscribeFrom } = req.body;
-
-  // Validate invitationids
-  if (!Array.isArray(invitationids)) {
-    const errMsg = "invitationids must be an array";
-    console.log(errMsg);
-    return res.status(400).send({
-      msg: errMsg,
-      msgType: "error",
-    });
-  }
 
   // Validate unsubscribeFrom
 
@@ -41,6 +124,18 @@ exports.POST = (req, res) => {
     });
   }
 
+  // Validate invitationids
+  if (unsubscribeFrom === "recipient") {
+    if (!Array.isArray(invitationids)) {
+      const errMsg = "invitationids must be an array";
+      console.log(errMsg);
+      return res.status(400).send({
+        msg: errMsg,
+        msgType: "error",
+      });
+    }
+  }
+
   // Validate jwt
 
   if (!jwt) {
@@ -52,58 +147,40 @@ exports.POST = (req, res) => {
     });
   }
 
-  jsonwebtoken.verify(jwt, process.env.INVITES_HMAC_SECRET, (err, jwtData) => {
-    if (err) {
-      return res.status(403).send({
-        msg: "invalid unsubscribe token",
-        msgType: "error",
-        err: err,
-      });
-    }
-
-    const { invitationid, userid } = jwtData;
-
-    if (!invitationids.includes(invitationid)) {
-      const errMsg = "invitationid must match the invitationid in the JWT";
-      console.log(errMsg);
-      return res.status(403).send({
-        msg: errMsg,
-        msgType: "error",
-      });
-    }
-
-    const sql = `
-      UPDATE
-        invitations
-      SET
-        unsubscribedFromEmail = 1,
-        unsubscribedFromEmailAt = UTC_TIMESTAMP(),
-        unsubscribedFromPush = 1,
-        unsubscribedFromPushAt = UTC_TIMESTAMP()
-      WHERE
-        userid = ?
-      AND
-        invitationid IN ?
-    `;
-
-    db.query(sql, [userid, [invitationids]], (error, result) => {
-      if (error) {
-        console.log(error);
-        return res.status(500).send({
-          msg: "unable to unsubscribe",
+  jsonwebtoken.verify(
+    jwt,
+    process.env.INVITES_HMAC_SECRET,
+    async (err, jwtData) => {
+      if (err) {
+        return res.status(403).send({
+          msg: "invalid unsubscribe token",
           msgType: "error",
+          err: err,
         });
       }
 
-      return res.status(200).send({
-        msg: "unsubscription successful",
-        msgType: "success",
-      });
-    });
-  });
+      const { invitationid, userid } = jwtData;
 
-  /* return res.status(200).send({
-    msg: "unsubscribe successful",
-    msgType: "success",
-  }); */
+      let unsubscription;
+
+      if (unsubscribeFrom === "invite") {
+        unsubscription = await unsubscribeFromInvite(db, invitationid, userid);
+      } else if (unsubscribeFrom === "recipient") {
+        unsubscription = await unsubscribeFromRecipient(
+          db,
+          invitationids,
+          invitationid,
+          userid
+        );
+      } else if (unsubscribeFrom === "app") {
+        unsubscription = await unsubscribeFromApp(db, userid);
+      }
+
+      return res.status(200).send({
+        msg: "unsubscribe successful",
+        msgType: "success",
+        result: unsubscription,
+      });
+    }
+  );
 };
