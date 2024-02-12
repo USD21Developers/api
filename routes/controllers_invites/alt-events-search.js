e33c7ea5a0a7a58661bd61efa1a0897140052c28
Jason McNeill <vrtjason@gmail.com>
2024-02-11 23:35:50 -0700

    Build out events search API

--- routes/controllers_invites/alt-events-search.js
+++ routes/controllers_invites/alt-events-search.js
@@ -1,3 +1,25 @@
+function calculateZoomLevel(map, markers, origin, radius) {
+  var bounds = new google.maps.LatLngBounds();
+  bounds.extend(origin); // Extend bounds to include origin
+
+  markers.forEach(function (marker) {
+    bounds.extend(marker.getPosition());
+  });
+
+  var center = bounds.getCenter();
+  var distance = google.maps.geometry.spherical.computeDistanceBetween(
+    center,
+    bounds.getNorthEast()
+  );
+
+  var zoomLevel = Math.floor(
+    16 -
+      Math.log2(((40075016.686 / (2 * radius)) * Math.sqrt(2) * distance) / 360)
+  );
+
+  return zoomLevel;
+}
+
 exports.POST = (req, res) => {
   // Set database
   const isStaging =
@@ -11,10 +33,12 @@ exports.POST = (req, res) => {
   const userid = Number(req.body.userid) || null;
   const recipientid = req.body.recipientid || null;
   const countryFromIP = req.body.countryFromIP;
-  const distanceUnit = req.body.distanceUnit;
   const lang = req.body.lang;
   const originLocation = req.body.originLocation;
   const radius = req.body.radius;
+  const distanceUnit = req.body.distanceUnit;
+  const dateFromUTC = req.body.dateFromUTC;
+  const dateToUTC = req.body.dateToUTC;
 
   // MAIN LOGIC
 
