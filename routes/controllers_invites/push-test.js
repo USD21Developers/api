5af795fbfdbaa13fdb5f312227c26d320cf26d7e
Jason McNeill <vrtjason@gmail.com>
2024-04-29 09:05:43 -0700

    Change logic for web push test

--- routes/controllers_invites/push-test.js
+++ routes/controllers_invites/push-test.js
@@ -50,14 +50,71 @@ exports.POST = async (req, res) => {
       });
     }
 
-    sendWebPush(db, req.user.userid, "Test", "This is a test push message", {
-      jason: "is cool",
-    })
-      .then((result) => {
-        // console.log(result);
+    const invitationid = isStaging ? 167 : 3;
+
+    const sql = `
+      SELECT
+        i.recipientname,
+        e.title,
+        e.type
+      FROM
+        invitations i
+      INNER JOIN events e ON i.eventid = e.eventid
+      WHERE
+        i.userid = ?
+      AND
+        i.invitationid = ?
+      LIMIT
+        1
+      ;
+    `;
+
+    db.query(sql, [req.user.userid, invitationid], (error, result) => {
+      if (error) {
+        return res.status(500).send({
+          msg: "unable to query for invitation",
+          msgType: "error",
+        });
+      }
+
+      if (!result.length) {
+        return res.status(404).send({
+          msg: "no record found for this invite",
+          msgType: "error",
+        });
+      }
+
+      const recipientName = result[0].recipientname;
+      const eventTitle = result[0].title;
+      const eventType = result[0].type;
+      let eventName = eventTitle;
+      if (eventType === "church") eventName = "church";
+      if (eventType === "bible talk") eventName = "Bible Talk";
+
+      const pushTitle = `${recipientName} clicked on your invite to ${eventName}`;
+      const pushBody = `Click here to follow up with ${recipientName}`;
+      const followUpURL = isStaging
+        ? `https://staging.invites.mobi/r/#/${invitationid}`
+        : `http://localhost:5555/r/#/${invitationid}`;
+
+      sendWebPush(db, req.user.userid, pushTitle, pushBody, {
+        followUpURL: followUpURL,
       })
-      .catch((error) => {
-        console.log(error);
-      });
+        .then((result) => {
+          return res.status(200).send({
+            msg: "test push message sent",
+            msgType: "success",
+            result: result,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          return res.status(500).send({
+            msg: "could not send test push message",
+            msgType: "error",
+            error: error,
+          });
+        });
+    });
   });
 };
