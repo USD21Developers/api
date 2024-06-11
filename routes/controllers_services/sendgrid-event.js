3d445ebc268fc70efe65822ad3af5ff2b8f4d275
Jason McNeill <vrtjason@gmail.com>
2024-06-10 21:29:59 -0700

    Create route to track SendGrid events

--- routes/controllers_services/sendgrid-event.js
+++ routes/controllers_services/sendgrid-event.js
@@ -1,13 +1,8 @@
-exports.GET = (req, res) => {
-  // const db = require("../../database-services");
-  // const sql = ``;
-
-  var events = req.body;
-  events.forEach(function (event) {
-    // Here, you now have each event and can process them how you like
-    // processEvent(event);
-    console.log(event);
-  });
+exports.POST = (req, res) => {
+  // Set database
+  const isLocal = req.headers.referer.indexOf("localhost") >= 0 ? true : false;
+  const isStaging = req.headers.referer.indexOf("staging") >= 0 ? true : false;
+  const db = require("../../database-services");
 
   return res.status(200).send();
 };
