cdcceb4fe90ede9e90e77d77cd120f7564a3666e
Jason McNeill <vrtjason@gmail.com>
2024-06-10 21:40:37 -0700

    Prepare to receive SendGrid events

--- routes/controllers_services/sendgrid-event.js
+++ routes/controllers_services/sendgrid-event.js
@@ -4,5 +4,15 @@ exports.POST = (req, res) => {
   const isStaging = req.headers.referer.indexOf("staging") >= 0 ? true : false;
   const db = require("../../database-services");
 
+  const processEvent = (event) => {
+    console.log(event);
+  };
+
+  var events = req.body;
+  events.forEach(function (event) {
+    // Here, you now have each event and can process them how you like
+    processEvent(event);
+  });
+
   return res.status(200).send();
 };
