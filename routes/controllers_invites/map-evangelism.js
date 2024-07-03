fc3643cf961e9c94617c5925ae0fff7a4793d22c
Jason McNeill <vrtjason@gmail.com>
2024-07-03 13:58:02 -0700

    Build out API to get map markers for user

---
+++ routes/controllers_invites/map-evangelism.js
@@ -0,0 +1,29 @@
+exports.POST = async (req, res) => {
+  // Enforce authorization
+  const usertype = req.user.usertype;
+  const allowedUsertypes = ["sysadmin", "user"];
+  let isAuthorized = false;
+  if (allowedUsertypes.includes(usertype)) isAuthorized = true;
+  if (!isAuthorized) {
+    console.log(`User (userid ${req.user.userid}) is not authorized.`);
+    return res.status(401).send({
+      msg: "user is not authorized for this action",
+      msgType: "error",
+    });
+  }
+
+  // Set database
+  const isStaging = req.headers.referer.indexOf("staging") >= 0 ? true : false;
+  const db = isStaging
+    ? require("../../database-invites-test")
+    : require("../../database-invites");
+  
+  // Params
+  const region = req.body.region || "us";
+  const language = req.body.language || "en";
+
+  return res.status(200).send({
+    msg: "evangelism map url signed",
+    msgType: "success"
+  });
+};
