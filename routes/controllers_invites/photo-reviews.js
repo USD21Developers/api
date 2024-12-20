c6cd9f2842c56045a384d1ec5cc15e4c1831785d
Jason McNeill <vrtjason@gmail.com>
2024-12-19 17:16:03 -0700

    Build out processing script

--- routes/controllers_invites/photo-reviews.js
+++ routes/controllers_invites/photo-reviews.js
@@ -22,6 +22,9 @@ exports.POST = (req, res) => {
   const userIdsApproved = req.body.userIdsApproved;
   const userIdsFlagged = req.body.userIdsFlagged;
   const photosFlagged = req.body.photosFlagged;
+  const htmlYourPhotoWasFlagged = req.body.htmlYourPhotoWasFlagged;
+  const emailPhrasesPhotoWasFlagged = req.body.emailPhrasesPhotoWasFlagged;
+  const adminTimeZone = req.body.adminTimeZone;
 
   // Variables
   const genericPhotoUrls = {
@@ -29,9 +32,158 @@ exports.POST = (req, res) => {
     female: "https://invites.mobi/_assets/img/profile-generic-female.png",
   };
 
-  const notifyFlaggedUser = (user) => {
+  const notifyFlaggedUser = (
+    db,
+    user,
+    htmlYourPhotoWasFlagged,
+    emailPhrasesPhotoWasFlagged,
+    adminTimeZone
+  ) => {
     return new Promise((resolve, reject) => {
       const { userid, reason, other, gender } = user;
+      const {
+        emailP1,
+        emailP2,
+        emailP3,
+        emailP4,
+        emailP5,
+        emailP6,
+        emailP7,
+        emailP8,
+        emailUpdatePhotoLink,
+        emailP10,
+        emailSincerely,
+        emailSubject,
+        emailTheCyberministry,
+        emailAboutApp,
+        emailTimezone,
+        emailMessageID,
+        photoRules,
+      } = emailPhrasesPhotoWasFlagged;
+      const {
+        headlineRulesAboutPhotos,
+        ruleMustShowYourFace,
+        explanationMustShowYourFace,
+        ruleFaceMustBeProminent,
+        explanationFaceMustBeProminent,
+        ruleOnlyYou,
+        explanationOnlyYou,
+        ruleMustBeAppropriate,
+        explanationMustBeAppropriate,
+      } = photoRules;
+      const jsdom = require("jsdom");
+      const { JSDOM } = jsdom;
+      const { document } = new JSDOM(htmlYourPhotoWasFlagged).window;
+      const tempPhotoURL =
+        gender === "female" ? genericPhotoUrls.female : genericPhotoUrls.male;
+      const uuid = require("uuid");
+      const messageID = uuid.v4();
+
+      // TODO: query the DB to populate the userInfo object below, then continue
+
+      sql = `
+        SELECT
+          u.firstname,
+          u.lastname,
+          u.profilephoto,
+          pr.createdAt AS profilePhotoDate
+        FROM
+          users u
+        INNER JOIN photoreview pr ON u.userid = pr.userid
+        WHERE
+          u.userid = ?
+        LIMIT 1
+        ;
+      `;
+
+      db.query(sql, [userid], (error, result) => {
+        if (error) {
+          console.log(error);
+          return reject(error);
+        }
+
+        if (!result.length) {
+          return reject(new Error("user not found"));
+        }
+
+        const userInfo = {
+          firstName: result[0].firstname,
+          lastName: result[0].lastname,
+          profilephoto: result[0].profilephoto,
+          profilePhotoDate: result[0].profilePhotoDate,
+        };
+
+        document.title = emailSubject;
+        document.querySelector(
+          "[data-i18n='profile-photo-was-flagged']"
+        ).innerHTML = emailSubject;
+        document.querySelector("[data-i18n='emailP1']").innerHTML =
+          emailP1.replaceAll("{FIRST-NAME}", userInfo.firstName);
+        document.querySelector("[data-i18n='emailP2']").innerHTML = emailP2;
+        document.querySelector("[data-i18n='emailP3']").innerHTML = emailP3;
+        document
+          .querySelector("[data-i18n='emailP3']")
+          .parentElement.querySelector("strong").innerHTML = reason;
+        document.querySelector(
+          "[data-i18n='headlineRulesAboutPhotos']"
+        ).innerHTML = headlineRulesAboutPhotos;
+        document.querySelector("[data-i18n='ruleMustShowYourFace']").innerHTML =
+          ruleMustShowYourFace;
+        document.querySelector(
+          "[data-i18n='explanationMustShowYourFace']"
+        ).innerHTML = explanationMustShowYourFace;
+        document.querySelector(
+          "[data-i18n='ruleFaceMustBeProminent']"
+        ).innerHTML = ruleFaceMustBeProminent;
+        document.querySelector(
+          "[data-i18n='explanationFaceMustBeProminent']"
+        ).innerHTML = explanationFaceMustBeProminent;
+        document.querySelector("[data-i18n='ruleOnlyYou']").innerHTML =
+          ruleOnlyYou;
+        document.querySelector("[data-i18n='explanationOnlyYou']").innerHTML =
+          explanationOnlyYou;
+        document.querySelector(
+          "[data-i18n='ruleMustBeAppropriate']"
+        ).innerHTML = ruleMustBeAppropriate;
+        document.querySelector(
+          "[data-i18n='explanationMustBeAppropriate']"
+        ).innerHTML = explanationMustBeAppropriate;
+        document.querySelector("[data-i18n='emailP4']").innerHTML = emailP4;
+        document.querySelector("[data-i18n='emailP5']").innerHTML = emailP5;
+        document
+          .querySelector("#flaggedPhoto")
+          .setAttribute("src", userInfo.profilephoto);
+        document.querySelector("[data-i18n='addedOn']").innerHTML =
+          photoAddedOn.replaceAll("{DATE}", userInfo.profilePhotoDate);
+        document.querySelector("[data-i18n='emailP6']").innerHTML = emailP6;
+        document.querySelector("[data-i18n='emailP7']").innerHTML = emailP7;
+        document
+          .querySelector("#temporaryPhoto")
+          .setAttribute("src", tempPhotoURL);
+        document.querySelector("[data-i18n='emailP8']").innerHTML = emailP8;
+        document.querySelector("[data-i18n='emailUpdatePhotoLink']").innerHTML =
+          emailUpdatePhotoLink;
+        document.querySelector("[data-i18n='emailP10']").innerHTML = emailP10;
+        document.querySelector("[data-i18n='emailSincerely']").innerHTML =
+          emailSincerely;
+        document.querySelector(
+          "[data-i18n='emailTheCyberministry']"
+        ).innerHTML = emailTheCyberministry;
+        document.querySelector("[data-i18n='emailAboutApp']").innerHTML =
+          emailAboutApp;
+        document.querySelector("[data-i18n='emailTimezone']").innerHTML =
+          emailTimezone.replaceAll("{ADMIN-TIMEZONE}", adminTimeZone);
+        document.querySelector("[data-i18n='emailMessageID']").innerHTML =
+          emailMessageID;
+        document.querySelector("[data-var='messageID']").innerHTML = messageID;
+      });
+
+      const html = document.innerHTML;
+
+      // TODO:  make sure that the "html" variable contains the full HTML document
+      debugger;
+
+      // TODO:  send e-mail
     });
   };
 
@@ -72,12 +224,23 @@ exports.POST = (req, res) => {
         return resolve();
       }
 
+      const notificationPromises = [];
+
+      photosFlagged.forEach((user) => {
+        const notificationPromise = notifyFlaggedUser(
+          db,
+          user,
+          htmlYourPhotoWasFlagged,
+          emailPhrasesPhotoWasFlagged,
+          adminTimeZone
+        );
+        notificationPromises.push(notificationPromise);
+      });
+
       let caseProfilePhotoFlagged = "CASE ";
       let caseProfilePhoto = "CASE ";
 
-      photosFlagged.forEach((user) => {
-        notifyFlaggedUser(user);
-
+      Promise.allSettled(notificationPromises).then((results) => {
         caseProfilePhotoFlagged += `WHEN userid = ${user.userid} THEN profilephoto `;
         caseProfilePhoto += `WHEN userid = ${user.userid} THEN 
           (CASE 
