ab5b4ad1e685851c5502bced5b5df2b600319547
Jason McNeill <vrtjason@gmail.com>
2025-01-20 18:29:17 -0700

    Remove authentication requirement

--- routes/controllers_invites/churches-with-users.js
+++ routes/controllers_invites/churches-with-users.js
@@ -1,17 +1,4 @@
 exports.GET = async (req, res) => {
-  // Enforce authorization
-  const usertype = req.user.usertype;
-  const allowedUsertypes = ["sysadmin", "user"];
-  let isAuthorized = false;
-  if (allowedUsertypes.includes(usertype)) isAuthorized = true;
-  if (!isAuthorized) {
-    console.log(`User (userid ${req.user.userid}) is not authorized.`);
-    return res.status(401).send({
-      msg: "user is not authorized for this action",
-      msgType: "error",
-    });
-  }
-
   // Set database
   const isStaging = req.headers.referer.indexOf("staging") >= 0 ? true : false;
   const db = isStaging
