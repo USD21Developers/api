652f4e56285f586e59debf10716d639000f4b65c
Jason McNeill <vrtjason@gmail.com>
2025-01-24 14:11:31 -0700

    Add data to response - churches with registered users

--- routes/controllers_invites/sync-churches.js
+++ routes/controllers_invites/sync-churches.js
@@ -38,10 +38,45 @@ exports.GET = async (req, res) => {
     const oneWeek = 604800;
     cache.set("churches", result, oneWeek);
 
-    return res.status(200).send({
-      msg: "churches retrieved",
-      msgType: "success",
-      churches: result,
+    const churches = result;
+
+    const sql = `
+      SELECT 
+        DISTINCT u.churchid,
+        c.name,
+        c.place,
+        c.country,
+        c.url
+      FROM users u
+      INNER JOIN churches c ON u.churchid = c.remoteid
+      WHERE
+        u.churchid > 0
+      AND
+        u.userstatus = 'registered'
+      AND
+        c.isDeleted <> 1
+      ORDER BY
+        country, place, name
+      ;
+    `;
+
+    db.query(sql, [], (error, result) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send({
+          msg: "unable to query for churches with registered users",
+          msgType: "error",
+        });
+      }
+
+      const churchesWithRegisteredUsers = result;
+
+      return res.status(200).send({
+        msg: "churches retrieved",
+        msgType: "success",
+        churches: churches,
+        churchesWithRegisteredUsers: churchesWithRegisteredUsers,
+      });
     });
   });
 };
