54156c8409e00eb6ce7d99bb262247ca81f60576
Jason McNeill <vrtjason@gmail.com>
2025-02-03 18:24:14 -0700

    Fix bug

--- routes/controllers_invites/admin-user-update.js
+++ routes/controllers_invites/admin-user-update.js
@@ -606,7 +606,7 @@ exports.POST = async (req, res) => {
 
   // Admins that are not super users cannot edit users from other congregations
   if (!isSuperUser) {
-    if (sysAdminMe.churchid !== user.churchid) {
+    if (sysAdminMe.churchid != user.churchid) {
       return res.status(400).send({
         msg: "insufficient permissions to modify a user from another congregation",
         msgType: "error",
